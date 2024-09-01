7f3b634955d18c7c5e84f05020c94f3a41571367
Samantha Trujillo <samantha.trujillo@unmsm.edu.pe>
2024-09-01 14:16:34 -0500

    tarea sesión 03 completada

--- Sesion_03/Test03.py
+++ Sesion_03/Test03.py
@@ -4,3 +4,9 @@ print("La edad del usuario {} es {}.".format(nombre,edad))
 
 actualizar=int(edad)+5
 print("Su edad en 5 años será: {}.".format(actualizar))
+
+milista=['Python','Backend','20 años']
+nombretecnologia,tipo,antiguedad=milista
+print("Nombre de la tecnología {}".format(nombretecnologia))
+print("Tipo de tecnología {}".format(tipo))
+print("Antiguedad {}".format(antiguedad))
---
+++ Sesion_03/Test04.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+var1=input("Ingrese el nombre del empleado: ")
+var2=input("Ingrese el apellido del empleado: ")
+var3=input("Ingrese su distrito de residencia: ")
+var4=int(input("Ingrese el monto de su sueldo: "))
+dicci1={}
+dicci1['nombre']=var1
+dicci1['apellido']=var2
+dicci1['distrito']=var3
+dicci1['sueldo']=var4
+bonof=dicci1['sueldo']*3-0.1*dicci1['sueldo']
+dicci1['bonificación']=bonof
+nombre=dicci1['nombre']
+apellido=dicci1['apellido']
+boni=dicci1['bonificación']
+print("{} {}, recibirá bono de {} soles al fin de año.".format(nombre,apellido,boni))
