c6e471f678be0e3e82b9a0524f0b570a626f2314
Samantha Trujillo <samantha.trujillo@unmsm.edu.pe>
2024-09-01 13:49:32 -0500

    último ejercicio sesión 03

---
+++ Sesion_03/Test03.py
@@ -0,0 +1,6 @@
+nombre=input("Ingrese su nomnbre: ")
+edad=input("Ingrese su edad: ")
+print("La edad del usuario {} es {}.".format(nombre,edad))
+
+actualizar=int(edad)+5
+print("Su edad en 5 años será: {}.".format(actualizar))
